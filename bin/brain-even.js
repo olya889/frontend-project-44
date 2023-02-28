6e48e5d8c39d83af159e6646045eb4ffde8d6d20
Olga Efremova <pcholyk@yandex.ru>
2023-02-28 23:01:28 +0300

    move evenGame function in even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,30 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import evenGame from '../src/games/even.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-let i = 0;
-while (i < 3) {
-  const randomNumber = Math.round((Math.random() * 10));
-  console.log(`Question: ${randomNumber}`);
-  const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
-  const answer = readlineSync.question('Your answer: ');
-  if (answer === rightAnswer) {
-    console.log('Correct!');
-    i += 1;
-  } else {
-    /* if (answer === 'yes') {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-      break;
-    } */
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
-    break;
-  }
-  if (i === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
-}
+evenGame();
