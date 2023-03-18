6d3559136bfac151b80f58a63185d288d513bfde
Olga Efremova <pcholyk@yandex.ru>
2023-03-18 23:24:37 +0300

    replace const firstTerm, gap, numbersCount from getRandomSet in getQuestionsAndRightAnswers

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,23 +1,23 @@
 import startGame from '../index.js';
 import getRandomNumber from '../utils.js';
 
-const getRandomSet = () => {
-  const term = Math.round((getRandomNumber() * 10));
+const getRandomSet = (firstTerm, gap, numbersCount) => {
   const randomSet = [];
-  const gap = Math.ceil((getRandomNumber() * 10));
-  const minNumbersCount = 5;
-  const maxNumbersCount = 11;
-  const interval = maxNumbersCount - minNumbersCount;
-  const numbersQuantity = Math.floor(getRandomNumber() * (interval) + minNumbersCount);
-  for (let i = 1; i <= numbersQuantity; i += 1) {
-    const nextTerm = term + gap * i;
+  for (let i = 1; i <= numbersCount; i += 1) {
+    const nextTerm = firstTerm + gap * i;
     randomSet.push(nextTerm);
   }
   return randomSet;
 };
 
 const getQuestionsAndRightAnswers = () => {
-  const set = getRandomSet();
+  const firstTerm = Math.round((getRandomNumber() * 10));
+  const gap = Math.ceil((getRandomNumber() * 10));
+  const minNumbersCount = 5;
+  const maxNumbersCount = 11;
+  const interval = maxNumbersCount - minNumbersCount;
+  const numbersCount = Math.floor(getRandomNumber() * (interval) + minNumbersCount);
+  const set = getRandomSet(firstTerm, gap, numbersCount);
   const randomItem = Math.floor(getRandomNumber() * set.length);
   const rightAnswer = String(set[randomItem]);
   set[randomItem] = '..';
