23a437f78c1a8776636c869e304cf2bcbfd5b116
Olga Efremova <pcholyk@yandex.ru>
2023-03-18 23:12:31 +0300

    add constants in getRandomSet to make numbersQuantity calculation clear

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,7 +5,10 @@ const getRandomSet = () => {
   const term = Math.round((getRandomNumber() * 10));
   const randomSet = [];
   const gap = Math.ceil((getRandomNumber() * 10));
-  const numbersQuantity = Math.floor(getRandomNumber() * (10 - 5 + 1) + 5);
+  const minNumbersCount = 5;
+  const maxNumbersCount = 11;
+  const interval = maxNumbersCount - minNumbersCount;
+  const numbersQuantity = Math.floor(getRandomNumber() * (interval) + minNumbersCount);
   for (let i = 1; i <= numbersQuantity; i += 1) {
     const nextTerm = term + gap * i;
     randomSet.push(nextTerm);
