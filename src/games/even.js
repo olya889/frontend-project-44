72246c7cb1d6a9fa9c94f6c74699c50168bfa392
Olga Efremova <pcholyk@yandex.ru>
2023-03-18 22:35:57 +0300

    shorten isEven function

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,7 @@
 import startGame from '../index.js';
 import getRandomNumber from '../utils.js';
 
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return true;
-  } return false;
-};
+const isEven = (number) => number % 2 === 0;
 
 const getQuestionsAndRightAnswers = () => {
   const question = Math.round((getRandomNumber() * 10));
