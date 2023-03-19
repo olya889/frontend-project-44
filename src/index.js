79f105bbdf522a3cb7ce893bbac54717441059c6
Olga Efremova <pcholyk@yandex.ru>
2023-03-19 08:58:42 +0300

    add count of rightanswers

--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ const startGame = (task, getQuestionsAndRightAnswers) => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(task);
+  let count = 0;
   for (let i = 0; i < 3; i += 1) {
     const [question, rightAnswer] = getQuestionsAndRightAnswers();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === rightAnswer) {
       console.log('Correct!');
+      count += 1;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
       break;
     }
-    if (i === 2) {
-      console.log(`Congratulations, ${name}!`);
-    }
+  }
+  if (count === 3) {
+    console.log(`Congratulations, ${name}!`);
   }
 };
 
